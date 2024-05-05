ceacfa0fae5202e03d2e2adf61cb9b20b38611cf
Mrinal Pramanick <mrinalbitsat@gmail.com>
2024-05-05 23:00:23 +0530

    DS challenges in rust

--- README.md
+++
@@ -1,2 +0,0 @@
-# pm2-threader
-Node Thread worker examples from pm2 library to scale node applications beyond the single thread.
---
+++ src/main.rs
@@ -0,0 +1,21 @@
+use serde::{Deserialize, Serialize};
+
+#[derive(Debug, Clone, Serialize, Deserialize)]
+struct Message {
+    src: String,
+    #[serde(rename = "dest")]
+    dest: String,
+    body: Body,
+}
+#[derive(Debug, Clone, Serialize, Deserialize)]
+struct Body {
+    #[serde(rename = "type")]
+    ty: String,
+    #[serde(rename = "msg_id")]
+    id: Option<usize>,
+    in_reply_to: Option<usize>,
+}
+
+fn main() {
+    println!("Hello, world!");
+}
