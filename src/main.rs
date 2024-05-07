9a80ade468ef015cf2b3ef2eca611830281635a8
Mrinal Pramanick <mrinalbitsat@gmail.com>
2024-05-07 22:44:11 +0530

    Working on Malestrom protocol

--- src/main.rs
+++ src/main.rs
@@ -1,3 +1,6 @@
+use std::io::StdoutLock;
+
+use anyhow::{Context, Ok};
 use serde::{Deserialize, Serialize};
 
 #[derive(Debug, Clone, Serialize, Deserialize)]
@@ -14,8 +17,37 @@ struct Body {
     #[serde(rename = "msg_id")]
     id: Option<usize>,
     in_reply_to: Option<usize>,
+    #[serde(flatten)]
+    payload: Payload,
 }
 
-fn main() {
-    println!("Hello, world!");
+#[derive(Debug, Clone, Serialize, Deserialize)]
+#[serde(tag = "type")]
+#[serde(rename_all = "snake_case")]
+enum Payload {
+    Echo { echo: String },
+}
+
+struct EchoNode;
+
+impl EchoNode {
+    pub fn handle(
+        &mut self,
+        input: Message,
+        output: &mut serde_json::Serializer<StdoutLock>,
+    ) -> anyhow::Result<()> {
+        Ok(())
+    }
+}
+
+fn main() -> anyhow::Result<()> {
+    let stdin = std::io::stdin().lock();
+    let stdout = std::io::stdout().lock();
+
+    let inputs = serde_json::Deserializer::from_reader(stdin).into_iter::<Message>();
+    for input in inputs {
+        let input = input.context("Malestrom input could not be deserliased")?;
+    }
+
+    Ok(())
 }
