6a0199de49f10d650fc65a4fa8de99f65c855124
Mrinal Pramanick <mrinalbitsat@gmail.com>
2024-05-20 21:23:42 +0530

    Added new func

--- src/main.rs
+++ src/main.rs
@@ -25,17 +25,27 @@ struct Body {
 #[serde(tag = "type")]
 #[serde(rename_all = "snake_case")]
 enum Payload {
-    Echo { echo: String },
-    EchoOk { echo: String },
-
+    Echo {
+        echo: String,
+    },
+    EchoOk {
+        echo: String,
+    },
+    Init {
+        node_id: String,
+        node_ids: Vec<String>,
+    },
+    InitOk {
+        echo: String,
+    },
 }
 
-struct EchoNode{
+struct EchoNode {
     id: usize,
 }
 
 impl EchoNode {
-    pub fn handle(
+    pub fn step(
         &mut self,
         input: Message,
         output: &mut serde_json::Serializer<StdoutLock>,
@@ -43,17 +53,20 @@ impl EchoNode {
         match input.body.payload {
             Payload::Echo { echo } => {
                 let reply = Message {
-                    src : input.dst,
+                    src: input.dst,
                     dst: input.src,
-                    body : Body {
-                        id : Some(self.id),
-                        in_reply_to: Some(input.body.id),
-                        payload : Payload::EchoOk { echo },
+                    body: Body {
+                        id: Some(self.id),
+                        in_reply_to: input.body.id,
+                        payload: Payload::EchoOk { echo },
                         ty: todo!(),
                     },
                 };
+                reply
+                    .serialize(output)
+                    .context("serialize response to echo")?;
                 self.id += 1;
-            },
+            }
             Payload::EchoOk { .. } => {}
         }
         Ok(())
@@ -65,8 +78,14 @@ fn main() -> anyhow::Result<()> {
     let stdout = std::io::stdout().lock();
 
     let inputs = serde_json::Deserializer::from_reader(stdin).into_iter::<Message>();
+
+    let mut output = serde_json::Serializer::new(stdout);
+    let mut state = EchoNode { id: 0 };
     for input in inputs {
         let input = input.context("Malestrom input could not be deserliased")?;
+        state
+            .step(input, &mut output)
+            .context("Node step function failed")?;
     }
 
     Ok(())
